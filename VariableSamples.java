d380f7b9257bf60aa28ed831f95f249f5eda9a73
Muhammad Irfan <126837692+MuhammadIrfan2038@users.noreply.github.com>
2023-03-04 14:46:29 +0700

    Penambahan file VariableSamples.java

---
+++ VariableSamples.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Irfan030323;
+
+/**
+ *
+ * @author zikainu
+ */
+public class VariableSamples {
+    public static void main(String[] args){
+        //deklarasi a tipe data dengan nama varible
+        //result dan tipe data boolean
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        //option dan tipe data char
+        char option;
+        option = 'C'; //menandai sebagai option
+                
+        //deklarasi tipedata dengan nama variabel
+        //grade, double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade = 0.0;
+        
+    }
+    
+}
