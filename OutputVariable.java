41e8dd01789a0a975a7da08e2335184f1139cb6d
Muhammad Irfan <126837692+MuhammadIrfan2038@users.noreply.github.com>
2023-03-04 14:47:03 +0700

    Penambahan file OutputVariable.java

---
+++ OutputVariable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Irfan030323;
+
+/**
+ *
+ * @author zikainu
+ */
+public class OutputVariable
+{
+    public static void main( String[] args){
+        int value = 10;
+        char x;
+        x = 'A';
+        
+        System.out.println(value);
+        System.out.println("The value of x = " + x);
+    }
+}
